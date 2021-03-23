661bc233a8eec940d38f6fcaf96f13a793b33a2d
matej medvescek <matej.medvescek@tscng.org>
2021-03-23 17:46:09 +0100

    initial commit

---
+++ script/script.js
@@ -0,0 +1,41 @@
+var first= document.getElementById("content-one");
+var second = document.getElementById("content-two");
+var bisect = document.getElementById("second")
+var closeTwo = document.getElementById("close-two");
+/*var counter = 1;
+document.getElementById("right").addEventListener("click", ()=>{
+    if(counter%2 == 0){
+        first.style.display= "flex";
+        second.style.display = "none";
+        counter++;
+    }else{
+        second.style.display = "flex";
+        first.style.display = "none";
+        counter++;
+    }
+});
+document.getElementById("left").addEventListener("click", ()=>{
+    if(counter%2 == 0){
+        first.style.display= "flex";
+        second.style.display = "none";
+        counter++;
+    }else{
+        second.style.display = "flex";
+        first.style.display = "none";
+        counter++;
+    }
+});
+*/
+document.getElementById("second-button").addEventListener("click", ()=>{
+    console.log("lol")
+    bisect.style.display="flex";
+});
+closeTwo.addEventListener("click", ()=>{
+    console.log("close");
+    bisect.style.display="none";
+})
+
+document.getElementById("next").addEventListener("click", ()=>{
+    first.style.display="none";
+    second.style.display="flex";
+})
